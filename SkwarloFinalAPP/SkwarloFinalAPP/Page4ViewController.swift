6af2cd35ab9d649b3997a47d8faccd26915e1804
ZaneS27 <zane.skwarlo@gmail.com>
2021-07-24 11:32:19 -0500

    Images uploaded and such
    App finished

---
+++ SkwarloFinalAPP/SkwarloFinalAPP/Page4ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Page4ViewController.swift
+//  SkwarloFinalAPP
+//
+//  Created by rvcstudent on 7/24/21.
+//
+
+import UIKit
+
+class Page4ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
